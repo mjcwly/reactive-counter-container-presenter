5c2a2b6bf4e6832a00c89f416bf21afa8d9bed4f
mjcwly <mjcwly@gmail.com>
2022-07-08 23:27:39 +0100

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { StepSelectionComponent } from './components/step-selection/step-selection.component';
 import { TickSettingsComponent } from './components/tick-settings/tick-settings.component';
 import { CounterComponent } from './components/counter/counter.component';
@@ -13,7 +12,6 @@ import { ManualControlsComponent } from './components/manual-controls/manual-con
   imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
-    HelloComponent,
     StepSelectionComponent,
     TickSettingsComponent,
     CounterComponent,
